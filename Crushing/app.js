823b45cfcc0b1d1bdff4515d2bbb315824705613
desean99 <jones.desean99@gmail.com>
2023-04-06 12:56:13 -0400

    Add files via upload

---
+++ Crushing/app.js
@@ -0,0 +1,29 @@
+const $btnYes = document.querySelector('.js-yes'),
+    $btnNo = document.querySelector('.js-no'),
+    $confirm = document.querySelector('.js-confirm'),
+    $dialog = document.querySelector('.js-dialog'),
+    $body = document.querySelector('body')
+
+let counter = 0
+
+$btnYes.addEventListener('mouseover', (e) => {
+    e.target.innerHTML = 'Lets Go!'
+})
+
+$btnYes.addEventListener('click', () => {
+    $body.classList.add('yes')
+})
+
+$btnNo.addEventListener('mouseover', (e) => {
+    const $target = e.target
+
+    $target.style.top = `${Math.random() * 200}px`
+    $target.style.right = `${Math.random() * 200}px`
+
+    if (counter >= 5) {
+        $target.innerHTML = 'Almost worked...'
+    }
+
+    counter += 1
+
+})
